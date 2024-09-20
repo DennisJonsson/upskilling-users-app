ce7c666ed8621856dfbd91b8559290830721e281
Dennis Jönsson <dennis.jonsson16@ingka.ikea.com>
2024-09-20 14:51:17 +0200

    changed folder name

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const userRouters = require('./Routes/userRoutes');
+const express = require('express');
 const sequelize = require('./Config/database');
-const cors = require("cors");
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(express.static("react-frontend/dist"));
-
-app.use('/api', userRouters);
 
 sequelize.sync().then(() => {
     console.log("Database synched");
 }).catch((err) => {
     console.error("Error synching database: ", err);
 });
+
+app.use(cors());
+
+app.use(express.json());
+app.use('/',express.static(path.join(__dirname,'ikea-react-frontend/dist')));
+//app.use(express.static('ikea-react-frontend/dist'));
+
+const userRoutes = require('./Routes/userRoutes');
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, async () => {
